6d714be8249b487e9c569bb0e3c3ff28a93c0df1
franklee98 <frankli@rice.edu>
2019-09-16 22:37:00 -0500

    editted shot_scrape to make code more reproducible

--- shot_scrape.py
+++ shot_scrape.py
@@ -1,6 +1,4 @@
-# Credit to:
-# http://savvastjortjoglou.com/nba-shot-sharts.html
-# https://github.com/peaceiyi/NBAJSONCSV for how to get player ids
+# Credit to: https://github.com/peaceiyi/NBAJSONCSV for how to get player ids
 
 
 import requests
@@ -9,6 +7,8 @@ import pandas as pd
 
 # We focus on 2016-17 season here
 season = '2016-17'
+regular_season = 'Regular+Season'
+playoffs = 'Playoffs'
 
 
 def url_to_df(url):
@@ -30,7 +30,33 @@ def url_to_df(url):
     return df
 
 
-player_general_stats_1617_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=' \
+def url_to_shot_csv(general_stats_url, season_type):
+    """
+    :param general_stats_url: the url for getting general stats in order ot get player_id
+    :param season_type: regular season or playoffs
+    :return: a series of csv consisting of individual player's shots
+    """
+    df = url_to_df(general_stats_url)
+    player_id_df = df['PLAYER_ID']
+    for idx in range(len(player_id_df)):
+        player_shot_chart_url = 'https://stats.nba.com/stats/shotchartdetail?Period=0&VsConference=' \
+                                             '&LeagueID=00&LastNGames=0&TeamID=0&PlayerPosition=&Location=&Outcome=' \
+                                             '&ContextMeasure=FGA&DateFrom=&StartPeriod=&DateTo=&OpponentTeamID=0' \
+                                             '&ContextFilter=' \
+                                             '&RangeType=&' \
+                                             'Season=' + season + \
+                                             '&AheadBehind=' \
+                                             '&PlayerID=' + player_id_df[idx].astype(str) + \
+                                             '&EndRange=&VsDivision=' \
+                                             '&PointDiff=&RookieYear=&GameSegment=&Month=0&ClutchTime=' \
+                                             '&StartRange=&EndPeriod=' \
+                                             '&SeasonType=' + season_type + '&SeasonSegment=&GameID='
+        player_shot_chart_df = url_to_df(player_shot_chart_url)
+        player_shot_chart_df.to_csv('player_shot_chart_1617_' + season_type + '_' + player_id_df[idx].astype(str) +
+                                    '.csv')
+
+
+player_general_stats_1617_regular_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=' \
                                 '&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=' \
                                 '&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0' \
                                 '&OpponentTeamID=0' \
@@ -38,27 +64,17 @@ player_general_stats_1617_url = 'https://stats.nba.com/stats/leaguedashplayersta
                                 '&PlayerPosition=&PlusMinus=N&Rank=N' \
                                 '&Season=' + season + \
                                 '&SeasonSegment=' \
-                                '&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0' \
+                                '&SeasonType=' + regular_season + '&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0' \
                                 '&VsConference=&VsDivision=&Weight='
-player_general_stats_1617_df = url_to_df(player_general_stats_1617_url)
-# player_general_stats_1617_df.to_csv("player_list_general_stats_1617.csv")
+player_general_stats_1617_playoffs_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=' \
+                                '&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=' \
+                                '&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0' \
+                                '&OpponentTeamID=0' \
+                                '&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=' \
+                                '&PlayerPosition=&PlusMinus=N&Rank=N' \
+                                '&Season=' + season + \
+                                '&SeasonSegment=' \
+                                '&SeasonType=' + playoffs + '&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0' \
+                                '&VsConference=&VsDivision=&Weight='
 
-# Extract player_id, player_name, team
-player_info_1617_df = player_general_stats_1617_df[['PLAYER_ID', 'PLAYER_NAME', 'TEAM_ID', 'TEAM_ABBREVIATION']]
-# player_info_1617_df.to_csv('player_info_1617.csv')
-
-player_id_1617_df = player_general_stats_1617_df['PLAYER_ID']
-
-for i in range(len(player_info_1617_df)):
-    player_shot_chart_1617_url = 'https://stats.nba.com/stats/shotchartdetail?Period=0&VsConference=' \
-                 '&LeagueID=00&LastNGames=0&TeamID=0&PlayerPosition=&Location=&Outcome=' \
-                 '&ContextMeasure=FGA&DateFrom=&StartPeriod=&DateTo=&OpponentTeamID=0&ContextFilter=' \
-                 '&RangeType=&' \
-                 'Season=' + season + \
-                 '&AheadBehind=' \
-                 '&PlayerID=' + player_id_1617_df[i].astype(str) + \
-                 '&EndRange=&VsDivision=' \
-                 '&PointDiff=&RookieYear=&GameSegment=&Month=0&ClutchTime=&StartRange=&EndPeriod=' \
-                 '&SeasonType=Regular+Season&SeasonSegment=&GameID='
-    player_shot_chart_1617_df = url_to_df(player_shot_chart_1617_url)
-    # player_shot_chart_1617_df.to_csv('player_shot_chart_1617_' + player_id_1617_df[i].astype(str) + '.csv')
+url_to_shot_csv(player_general_stats_1617_playoffs_url, playoffs)
