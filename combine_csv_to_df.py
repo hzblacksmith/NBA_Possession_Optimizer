c30c83004c5d2aef648ad927184b8b4c36df8730
franklee98 <frankli@rice.edu>
2019-09-16 22:57:01 -0500

    update

--- combine_csv_to_df.py
+++ combine_csv_to_df.py
@@ -10,7 +10,8 @@ def combine_csv_to_df(path):
     return concatenated_df
 
 
-player_shot_1617_path = r'C:\Users\lilif\Desktop\NBA Hackathon\2019 Basketball Analytics\Basketball' \
-                        r'\player_shot_chart_1617'
-player_shot_1617_all_df = combine_csv_to_df(player_shot_1617_path)
-player_shot_1617_all_df.to_csv("player_shot_1617_all.csv")
+player_shot_1617_playoffs_path = r'C:\Users\lilif\Desktop\NBA Hackathon\2019 Basketball Analytics\Basketball' \
+                                 r'\player_shot_chart_1617\playoffs' \
+
+player_shot_1617_playoffs_df = combine_csv_to_df(player_shot_1617_playoffs_path)
+# player_shot_1617_playoffs_df.to_csv("player_shot_1617_Playoffs_all.csv")
