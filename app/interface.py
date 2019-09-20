15ebc36664e9710919196cf8edcb1faafb0eaf05
Skyler Shi <wwwskylercom@hotmail.com>
2019-09-20 09:22:10 -0500

    interface is interactive and somewhat styled

--- app/interface.py
+++ app/interface.py
@@ -12,41 +12,82 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-shotchart = pd.read_csv("Data_supplement/Shot_zones/bos_shot_1617.csv")
-
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-
 
 app.layout = html.Div(
-    style={'backgroundColor': colors['background'],
-            'marginLeft': 20, 'marginRight': 20, 'marginTop': 20, 'marginBottom': 20,
-            'border-radius': 10
-          },
+    id = 'main',
     children=[
-        html.H1(
-            children='Possession Optimizer for Coaches',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
+        html.Div(
+            id = 'info-select',
+            children = [
+                html.Div(
+                    id = 'info-select-sub',
+                    children = [
+                        html.P(
+                            children='POSSESSION OPTIMIZER',
+                            style = {
+                                'letter-spacing': 2.1,
+                                'font-weight': 700,
+                                'font-size': '170%',
+                            }
+                        ),
+                        dcc.Dropdown(
+                            id='game-select-dropdown',
+                            options=[
+                                {'label': 'Cle vs Bos 1', 'value': 'Cle_bos_1'},
+                                {'label': 'Cle vs Bos 2', 'value': 'Cle_bos_2'},
+                                {'label': 'Cle vs Bos 3', 'value': 'Cle_bos_3'},
+                            ],
+                            placeholder='Select Game',
+                            value='Null Game',
+                            searchable=False,
+                        ),
+                        dcc.Dropdown(
+                            id='possession-select-dropdown',
+                            options=[
+                                {'label': '{}'.format(i), 'value': i} for i in range(100)
+                            ],
+                            placeholder='Select Possession',
+                            value='Null',
+                            searchable=False,
+                        )
+                    ]
+                )
+            ]
         ),
-
-        dcc.Graph(
-            id='main-graph',
-            style = {
-                'width': '50%'
-            },
-            figure = go.Figure(
-                data = go.Scatter(
-                    x = shotchart['LOC_X'],
-                    y = shotchart['LOC_Y'],
-                    mode='markers'
+        html.Div(
+            id = 'interface',
+            children = [
+                html.Div(
+                    id = 'interface-top',
+                    children = [
+                        dcc.Graph(
+                            id='main-graph',
+                        )
+                    ]
                 ),
-                layout=go.Layout(
+                html.Div(
+                    id = 'interface-bottom',
+                    children = [
+                        dcc.Graph(
+                            id='main-graph-2',
+                        )
+                    ]
+                )
+            ]
+        )
+    ]
+)
+
+
+@app.callback(
+    Output('main-graph','figure'),
+    [Input('game-select-dropdown', 'value')]
+)
+def update_main_graph(value):
+    # initializaion: only draw the court
+    if (value == "Null Game"):
+        return {
+            'layout' : go.Layout(
                     xaxis=dict(showgrid=False,
                                range=[-300,300]),
                     yaxis=dict(showgrid=False,
@@ -54,10 +95,26 @@ app.layout = html.Div(
                     # draw the court
                     shapes = court_shapes
                 )
-            )
+        }
+
+    main_df = pd.read_csv("Data_supplement/Shot_zones_reg_season/bos_shot_1617.csv")
+
+
+    return {
+        'data' : [go.Scatter(
+                    x = main_df['LOC_X'],
+                    y = main_df['LOC_Y'],
+                    mode='markers'
+                )],
+        'layout' : go.Layout(
+            xaxis=dict(showgrid=False,
+                        range=[-300,300]),
+            yaxis=dict(showgrid=False,
+                        range=[422.5,-47.5]),
+            # draw the court
+            shapes = court_shapes
         )
-    ]
-)
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
